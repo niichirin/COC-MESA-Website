81b5faab16fda0dd8263f5ebb8ca3f45dde3c7dd
niichirin <itschristaguba@gmail.com>
2023-01-16 12:08:06 -0800

    Linked JavaScript to collapsible HTML

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+const collapsibles = document.querySelectorAll('.collapsible');
+
+collapsibles.forEach( (button) => {
+    button.addEventListener('click', toggleCollapsible);
+});
+
+function toggleCollapsible(e) {
+    
+}
