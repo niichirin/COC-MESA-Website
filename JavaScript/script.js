1036be246c78f47c8accb70996932fe8f4246687
niichirin <itschristaguba@gmail.com>
2023-01-16 12:47:30 -0800

    Added animation to collapsible content.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,12 +6,13 @@ collapsibles.forEach( (button) => {
 
 function toggleCollapsible(e) {
     let button = e.target;
-    button.classList.toggle('active');
+    button.classList.toggle('collapsible-active');
 
     let content = button.nextElementSibling;
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
+    if (content.style.maxHeight) { // if content shows
+        content.style.maxHeight = null; // hide it (remove height)
     } else {
-        content.style.display = 'block';
+        content.style.maxHeight = content.scrollHeight + "px";
+        // account for all visible and off-screen content with .scrollHeight
     }
 }
