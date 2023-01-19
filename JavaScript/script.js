88cf10f6637956c4ef74d97a2f713bd630fc832d
niichirin <itschristaguba@gmail.com>
2023-01-18 18:42:43 -0800

    Uncommented collapsible function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,35 @@
 const collapsibles = document.querySelectorAll('.collapsible');
-
 collapsibles.forEach( (button) => {
     button.addEventListener('click', toggleCollapsible);
 });
 
+/*
+const about = document.querySelector('#about-container');
+document.addEventListener('scroll', showAbout)
+function showAbout() {
+    let windowPos = window.scrollY; // position of window
+
+    let aboutPos = about.getBoundingClientRect().top;
+
+    if (aboutPos - windowPos < 0) {
+        about.style.opacity = '100%';
+        about.style.transform = 'translateY(0)';
+    }
+}
+
+const results = document.querySelector('#results-container')
+document.addEventListener('scroll', showResults)
+function showResults() {
+    let windowPos = window.scrollY; // position of window
+
+    let resultsPos = results.getBoundingClientRect().top;
+
+    if (resultsPos - windowPos < 0) {
+        results.style.opacity = '100%';
+        results.style.transform = 'translateY(0)';
+    }
+}
+*/
 function toggleCollapsible(e) {
     let button = e.target;
     button.classList.toggle('collapsible-active');
