cc8cd20d9712bb15b5258ae6a63396edea4547b7
ectaguba <113264419+niichirin@users.noreply.github.com>
2023-01-28 14:29:41 -0800

    Changed class name of collapsible buttons

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,11 @@
-const collapsibles = document.querySelectorAll('.collapsible');
-collapsibles.forEach( (button) => {
+const collapsibleBtns = document.querySelectorAll('.collapsible-btn');
+collapsibleBtns.forEach( (button) => {
     button.addEventListener('click', toggleCollapsible);
 });
 
 function toggleCollapsible(e) {
     let button = e.target;
-    button.classList.toggle('collapsible-active');
+    button.classList.toggle('collapsible-btn-active');
 
     let content = button.nextElementSibling;
     if (content.style.maxHeight) { // if content shows
