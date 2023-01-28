209024699224aa770e783879f6135b33869ebbd5
ectaguba <113264419+niichirin@users.noreply.github.com>
2023-01-28 14:44:27 -0800

    Commented old collapsible code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,7 @@ collapsibleBtns.forEach( (button) => {
     button.addEventListener('click', toggleCollapsible);
 });
 
+/*
 function toggleCollapsible(e) {
     let button = e.target;
     button.classList.toggle('collapsible-btn-active');
@@ -11,7 +12,9 @@ function toggleCollapsible(e) {
     if (content.style.maxHeight) { // if content shows
         content.style.maxHeight = null; // hide it (remove height)
     } else {
-        content.style.maxHeight = `${content.scrollHeight}px`;
+        content.style.maxHeight = `${content.style.height}px`;
+
         // account for all visible and off-screen content with .scrollHeight
     }
 }
+*/
