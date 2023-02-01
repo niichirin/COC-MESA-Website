3ac68f5e110e282bb106c4e4cd1d290f1fb141a7
ectaguba <113264419+niichirin@users.noreply.github.com>
2023-02-01 02:09:15 -0800

    Wrote styles and script for activating lab tab buttons

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,20 +7,42 @@ function openNavBar() {
     }
   }
 
-const skillsLabTabs = document.querySelectorAll('.lab-overview-tab');
-/* GUIDE for changing lab displays: https://www.w3schools.com/howto/howto_js_expanding_grid.asp */
-function openLab(newTab) {
-    skillsLabTabs.forEach( tab => {
-        if (tab.id != newTab) {
-            tab.style.display = 'none';
-        } else {
+const skillsLabBtns = document.querySelectorAll('.lab-overview-btn')
+skillsLabBtns.forEach( button => {
+    button.addEventListener("click", openLab);
+})
+
+function openLab(event) {
+    skillsLabBtns.forEach ( button => button.classList.remove('active')); // Deactivate all buttons
+    const pressedBtn = event.target; // Obtain button element
+    pressedBtn.classList.add('active') // Add active styles to pressedBtn
+
+    // Assign button text to corresponding tab id
+    const tabChoice = pressedBtn.textContent;
+    let newTab = '';
+    switch (tabChoice) {
+        case 'Biology/Chemistry': 
+            newTab = 'biology-tab';
+            break;
+        case 'Engineering': 
+            newTab = 'engineering-tab';
+            break;
+        case 'Physics': 
+            newTab = 'physics-tab';
+            break;
+        case 'Coding': 
+            newTab = 'coding-tab';
+            break;
+    }
+
+    // Get all tabs
+    const allTabs = document.querySelectorAll('.lab-overview-tab');
+    // Loop through tabs
+    allTabs.forEach( tab => {
+        if (tab.id == newTab) {
             tab.style.display = 'flex';
+        } else {
+            tab.style.display = 'none';
         }
     })
-}
-
-function closeLab(currentTab) {
-    skillsLabTabs.forEach( tab => {
-        if (tab.id == currentTab) tab.style.display = 'none';
-    });
 }
