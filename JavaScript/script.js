777fcb8b43d82bf6cdd9217c4e8551cd46bf4f4e
EmilioRRR308 <123784174+EmilioRRR308@users.noreply.github.com>
2023-02-09 21:09:12 -0800

    Built slideshows, descriptions for MESA tab

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,48 @@
-const collapsibles = document.querySelectorAll('.collapsible');
-collapsibles.forEach( (button) => {
-    button.addEventListener('click', toggleCollapsible);
-});
-
-function toggleCollapsible(e) {
-    let button = e.target;
-    button.classList.toggle('collapsible-active');
-
-    let content = button.nextElementSibling;
-    if (content.style.maxHeight) { // if content shows
-        content.style.maxHeight = null; // hide it (remove height)
+function openNavBar() {
+    let navbar = document.getElementById("primaryTopnav");
+    if (navbar.className === "topnav") {
+        navbar.className += " responsive";
     } else {
-        content.style.maxHeight = `${content.scrollHeight}px`;
-        // account for all visible and off-screen content with .scrollHeight
+        navbar.className = "topnav";
     }
+  }
+
+const skillsLabBtns = document.querySelectorAll('.lab-overview-btn')
+skillsLabBtns.forEach( button => {
+    button.addEventListener("click", openLab);
+})
+
+function openLab(event) {
+    skillsLabBtns.forEach ( button => button.classList.remove('active')); // Deactivate all buttons
+    const pressedBtn = event.target; // Obtain button element
+    pressedBtn.classList.add('active') // Add active styles to pressedBtn
+
+    // Assign button text to corresponding tab id
+    const tabChoice = pressedBtn.textContent;
+    let newTab = '';
+    switch (tabChoice) {
+        case 'Biology/Chemistry': 
+            newTab = 'biology-tab';
+            break;
+        case 'Engineering': 
+            newTab = 'engineering-tab';
+            break;
+        case 'Physics': 
+            newTab = 'physics-tab';
+            break;
+        case 'Coding': 
+            newTab = 'coding-tab';
+            break;
+    }
+
+    // Get all tabs
+    const allTabs = document.querySelectorAll('.lab-overview-tab');
+    // Loop through tabs
+    allTabs.forEach( tab => {
+        if (tab.id == newTab) {
+            tab.style.display = 'flex';
+        } else {
+            tab.style.display = 'none';
+        }
+    })
 }
