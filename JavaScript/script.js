8257d6d275e32182987b67dbac3e90273c654b5c
ectaguba <113264419+niichirin@users.noreply.github.com>
2023-06-20 13:20:12 -0700

    Created skills lab page + formatted styles.css

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,9 +9,10 @@ function isScrollingDown() {
   previousScrollPosition = scrollPosition;
   return goingDown;
 }
+
 // || NAVIGATION BAR FUNCTIONS
 const navbar = document.getElementById("primaryTopnav");
-const navHeight = navbar.getBoundingClientRect().height; 
+const navHeight = navbar.getBoundingClientRect().height; // using getBoundingClientRect().height makes navbar more modular in CSS (ex. easier to change padding and font size)
 
 function openNavBar() {
   if (navbar.className === "topnav") {
@@ -21,12 +22,7 @@ function openNavBar() {
   }
 }
 
-// important function to automatically displace fixed navbar
-// using getBoundingClientRect().height makes navbar more modular in CSS (ex. easier to change padding and font size)
 function displaceNavBar() {
-  // obtain navbar height
-  let navbar = document.querySelector("#primaryTopnav");
-
   // displace element after navbar
   let body = navbar.nextElementSibling;
   body.style.marginTop = `${navHeight}px`;
@@ -50,7 +46,7 @@ function moveNavOnScroll() {
 }
 window.addEventListener('scroll', moveNavOnScroll);
 
-// open nav menu, scroll up, scroll down, close nav menu
+// || SKILLS LAB (index.html)
 const skillsLabBtns = document.querySelectorAll('.lab-overview-btn')
 skillsLabBtns.forEach(button => {
   button.addEventListener("click", openLab);
@@ -115,41 +111,84 @@ for (var i = 0; i < coll.length; i++) {
   });
 }
 
+// function createCalendar(elem, year, month) {
+//   let mon = month - 1; // months in JS are 0..11, not 1..12
+//   let d = new Date(year, mon);
+//   let table = '<table><tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th><th>SU</th></tr><tr>';
+//   // spaces for the first row
+//   // from Monday till the first day of the month
+//   // * * * 1  2  3  4
+//   for (let i = 0; i < getDay(d); i++) {
+//     table += '<td></td>';
+//   }
+//   // <td> with actual dates
+//   while (d.getMonth() == mon) {
+//     table += '<td>' + d.getDate() + '</td>';
+//     if (getDay(d) % 7 == 6) { // sunday, last day of week - newline
+//       table += '</tr><tr>';
+//     }
+//     d.setDate(d.getDate() + 1);
+//   }
+//   // add spaces after last days of month for the last row
+//   // 29 30 31 * * * *
+//   if (getDay(d) != 0) {
+//     for (let i = getDay(d); i < 7; i++) {
+//       table += '<td></td>';
+//     }
+//   }
+//   // close the table
+//   table += '</tr></table>';
+//   elem.innerHTML = table;
+// }
 
-function createCalendar(elem, year, month) {
-  let mon = month - 1; // months in JS are 0..11, not 1..12
-  let d = new Date(year, mon);
-  let table = '<table><tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th><th>SU</th></tr><tr>';
-  // spaces for the first row
-  // from Monday till the first day of the month
-  // * * * 1  2  3  4
-  for (let i = 0; i < getDay(d); i++) {
-    table += '<td></td>';
-  }
-  // <td> with actual dates
-  while (d.getMonth() == mon) {
-    table += '<td>' + d.getDate() + '</td>';
-    if (getDay(d) % 7 == 6) { // sunday, last day of week - newline
-      table += '</tr><tr>';
-    }
-    d.setDate(d.getDate() + 1);
-  }
-  // add spaces after last days of month for the last row
-  // 29 30 31 * * * *
-  if (getDay(d) != 0) {
-    for (let i = getDay(d); i < 7; i++) {
-      table += '<td></td>';
-    }
-  }
-  // close the table
-  table += '</tr></table>';
-  elem.innerHTML = table;
+// function getDay(date) { // get day number from 0 (monday) to 6 (sunday)
+//   let day = date.getDay();
+//   if (day == 0) day = 7; // make Sunday (0) the last day
+//   return day - 1;
+// }
+
+// createCalendar(calendar, 2012, 9);
+
+// SKILLS LAB (skills-lab.html)
+let labSlidesContainers = document.getElementsByClassName("lab-slides-container");
+
+for (let i = 0; i < labSlidesContainers.length; i++) {
+  let slides = labSlidesContainers[i].getElementsByClassName("labSlides");
+  let slideIndex = 0;
+  showLabSlides(slides, slideIndex);
 }
 
-function getDay(date) { // get day number from 0 (monday) to 6 (sunday)
-  let day = date.getDay();
-  if (day == 0) day = 7; // make Sunday (0) the last day
-  return day - 1;
+function showLabSlides(slides, slideIndex) {
+  
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
 }
 
-createCalendar(calendar, 2012, 9);
+function changeLabSlide(element, n) {
+
+  let slideContainer = element.parentNode; // grab specific slideshow element
+  let slides = slideContainer.getElementsByClassName("labSlides");
+
+  // loop through slides to find active slide index
+  var slideIndex = Array.from(slides).findIndex(function (slide) {
+    return slide.style.display === "block";
+  });
+
+  slideIndex += n;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  } else if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  showLabSlides(slides, slideIndex);
+}
