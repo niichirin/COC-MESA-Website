501311ef10506c153ff91e3af4eebe7ad640d700
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-02-07 19:29:19 -0800

    implemented cors for external URL calling

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 // Entry point of API server.
 const express = require("express"); // API framework
-const connectDB = require("./config/db"); // PostgreSQL db
+const { connectDB } = require("./config/db"); // PostgreSQL db
 const routes = require("./routes/api/queries"); // API endpoints
+const cors = require("cors");
 const bodyParser = require("body-parser"); // JSON
 
 const app = express();
+
+// allow browser to connect to external URLs
+app.use(cors({ origin: true, credentials: true }));
 
 // set up JSON parser
 app.use(bodyParser.json());
