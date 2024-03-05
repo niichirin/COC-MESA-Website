b7ebba93b1f42f3531453fca9f765004a6d4bb73
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-03-05 01:19:48 -0800

    removed redundant imports

--- client/src/components/CreateTutor.tsx
+++ client/src/components/CreateTutor.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { useState } from 'react';
 import axios from 'axios';
 
 import BackToTutors from './BackToTutors';
