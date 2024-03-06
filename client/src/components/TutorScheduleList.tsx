a0bacf59e182098e1350d32c9f201ca9fb8d303a
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-03-06 01:36:56 -0800

    added function for converting sql time to normal time

--- client/src/components/TutorScheduleList.tsx
+++ client/src/components/TutorScheduleList.tsx
@@ -17,15 +17,42 @@ const TutorScheduleList: React.FC<TutorScheduleListProps> = ({ tutor }) => {
     // JSON in PSQL is unordered, this is here to order the iterations
     const daysOrder = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
+    // prevents schedule heading from rendering w/o values
+    let scheduleExists = false;
+
+    // convert SQL time to normal time
+    const convertSQLtime = (sqlTime: string) : string => {
+
+        // split time into hours, minutes, and seconds
+        const [hours, minutes, seconds] = sqlTime.split(':').map(Number);
+
+        // determine AM or Pm
+        const meridian = hours >= 12 ? 'PM' : 'AM';
+    
+        // convert hours to 12-hour format
+        let hours12 = hours % 12;
+
+        // handle 12:00 PM as 12:00 XM
+        hours12 = (hours12 === 0 ? 12 : hours12);
+    
+        // Format the time as xx:xx XM
+        const regularTime = `${hours12.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${meridian}`;
+    
+        return regularTime;
+    }
+
     // iterate through schedules of each day
     const renderedSchedule = daysOrder.map((key) => {
         // tutor doesn't teach on this day
         if (!tutor.schedule[key]?.length) return null; 
 
         // concat strings for each timeframe of the day
-        const times = tutor.schedule[key].map(
-            (item) => `${item.start} - ${item.end} (${item.location})`
-        );
+        const times = tutor.schedule[key].map((item) => {
+            if (item) {
+                scheduleExists = true;
+                return `${convertSQLtime(item.start)} - ${convertSQLtime(item.end)} (${item.location})`;
+            }
+        });
 
         // assign day string based on key
         let day = "";
@@ -65,7 +92,12 @@ const TutorScheduleList: React.FC<TutorScheduleListProps> = ({ tutor }) => {
         );
     });
 
-    return <><h3>Schedule</h3><div>{renderedSchedule}</div></>;
+    return (
+        scheduleExists && <>
+            <h3>Schedule</h3>
+            <div>{renderedSchedule.filter((item) => item !== null)}</div>
+        </>
+    );
 };
 
 export default TutorScheduleList;
