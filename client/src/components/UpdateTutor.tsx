3725592793af1781460b5f984954695982b2994d
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-03-06 18:26:39 -0800

    imported Tutor interface from Interfaces.ts

--- client/src/components/UpdateTutor.tsx
+++ client/src/components/UpdateTutor.tsx
@@ -1,45 +1,47 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import { Tutor, Course } from "./Interfaces.ts"
 import BackToTutors from "./BackToTutors";
+import UpdateTutorCourses from "./UpdateTutorCourses.tsx"
+import UpdateTutorSchedule from "./UpdateTutorSchedule.tsx";
 
 // updates tutor by ID from DB
-
-interface Tutor {
-    tutor_id: number, 
-    name: string, 
-    email: string 
-}
 
 const UpdateTutor = () => {
 
     const [tutor, setTutor] = useState<Tutor>();
+    const [courses, setCourses] = useState<{ [key: string]: Course }>();
     const [loading, setLoading] = useState(false);
 
-    const [newName, setNewName] = useState("");
-    const [newEmail, setNewEmail] = useState("");
+    const [newName, setNewName] = useState();
+    const [newEmail, setNewEmail] = useState();
 
     const { id } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
         setLoading(true);
-        axios
-            .get(`http://localhost:8082/api/tutoring/tutor/${id}`)
-            .then((res) => {
-                setTutor(res.data);
-                setNewName(res.data.name);
-                setNewEmail(res.data.email);
-            })
-            .catch((error) => console.error('Error updating tutor: ', error))
-            .finally(() => setLoading(false))
-    }, []);
+        Promise.all([
+            axios.get(`http://localhost:8082/api/tutoring/tutor/${id}`),
+            axios.get(`http://localhost:8082/api/tutoring/tutor/course/${id}`)
+        ])
+        .then(([tutorRes, coursesRes]) => {
+            setTutor(tutorRes.data)
+            setNewName(tutorRes.data.name)
+            setNewEmail(tutorRes.data.email)
+            setCourses(coursesRes.data)
+        })
+        .catch((error) => console.error('Error reading tutor: ', error))
+        .finally(() => setLoading(false))
+    }, [id]);
+
 
     const handleUpdate = () => {
         setLoading(true);
-        axios  
-            .put(`http://localhost:8082/api/tutoring/tutor/${id}`, { name: newName, email: newEmail } )
+        axios
+            .put(`http://localhost:8082/api/tutoring/tutor/${id}`, { name: newName, email: newEmail })
             .then((res) => {
                 console.log(res.data);
                 navigate(`/read-tutor/${id}`);
@@ -65,31 +67,33 @@ const UpdateTutor = () => {
         <div>
             <BackToTutors />
             <form className="TutorCard" onSubmit={handleUpdate}>
-            <label>
-                <b>Name: </b>
-                <input 
-                    type="text"
-                    value={newName}
-                    onChange={handleChangeName}
-                />
-            </label>
-            <br></br>
-            <label>
-                <b>Email: </b> 
-                <input 
-                    type="text"
-                    value={newEmail}
-                    onChange={handleChangeEmail}
-                /> 
-            </label>
-            <br></br>
-            <button 
-                style={{marginTop: "0.5rem"}}
-                type="submit"
-            >
-                Update
-            </button>
-        </form>
+                <label>
+                    <b>Name: </b>
+                    <input
+                        type="text"
+                        value={newName}
+                        onChange={handleChangeName}
+                    />
+                </label>
+                <br></br>
+                <label>
+                    <b>Email: </b>
+                    <input
+                        type="text"
+                        value={newEmail}
+                        onChange={handleChangeEmail}
+                    />
+                </label>
+                <br></br>
+                <UpdateTutorCourses courses={courses}/>
+                <UpdateTutorSchedule schedule={tutor.schedule}/>
+                <button
+                    style={{ marginTop: "0.5rem" }}
+                    type="submit"
+                >
+                    Update
+                </button>
+            </form>
         </div>
     )
 }
