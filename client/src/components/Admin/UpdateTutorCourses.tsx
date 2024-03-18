c4f672ab1ec662da97f03c619e3f266a83e5922a
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-03-17 20:28:38 -0700

    Updated courses state when adding new course

--- client/src/components/Admin/UpdateTutorCourses.tsx
+++ client/src/components/Admin/UpdateTutorCourses.tsx
@@ -12,14 +12,31 @@ const UpdateTutorCourses: React.FC<Props> = ({ courses }) => {
     const [coursesState, setCoursesState] = useState(courses);
     
     const handleAddToCourses = () => {
-    }
+        if (!coursesState) return;
+
+        const newCourseId = Object.keys(coursesState).length - 1;
+        const lastCourse = coursesState[newCourseId];
+        const newCourse: Course = {
+            course_id: lastCourse.course_id,
+            name: lastCourse.name,
+            subject: lastCourse.subject,
+            number: lastCourse.number
+        };
+
+        const newCourses = { 
+            ...coursesState, 
+            [newCourseId + 1]: newCourse 
+        };
+        
+        setCoursesState(newCourses);
+    };
 
     return <div className="bg-neutral-800 rounded px-4 py-2">
         <h3 className="font-bold mb-2">Courses</h3>
-        {!courses && <p>None</p>}
-        {courses && Object.keys(courses).map((key, index) => 
+        {!coursesState && <p>None</p>}
+        {coursesState && Object.keys(coursesState).map((key, index) => 
             <>
-                <CourseInput key={index} inputCourse={courses[key]}/>
+                <CourseInput key={index} inputCourse={coursesState[key]}/>
                 <br></br>
             </>
         )}
