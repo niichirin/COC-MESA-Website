f13da4d885e7ced03b22bd4cce5e1fcde78303a2
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-03-17 17:44:05 -0700

    Matched basename of App.jsx router to Vite config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from "./components/HomePage/HomePage";
 import ReadTutor from "./components/Admin/ReadTutor";
@@ -8,37 +6,31 @@ import ReadTutorsList from "./components/Admin/ReadTutorsList";
 import CreateTutor from "./components/Admin/CreateTutor";
 import UpdateTutor from "./components/Admin/UpdateTutor";
 
-// require('dotenv').config()
-
-// const repoName = process.env.REPO_NAME;
-// const router = createBrowserRouter([
-//   { path: `${repoName}/`, element: <HomePage /> },
-//   { path: `${repoName}/tutors`, element: <ReadTutorsList /> },
-//   { path: `${repoName}/create-tutor`, element: <CreateTutor /> },
-//   { path: `${repoName}/read-tutor/:id`, element: <ReadTutor /> },
-//   { path: `${repoName}/update-tutor/:id`, element: <UpdateTutor/> },
-// ]);
-
-// const router = createBrowserRouter([
-//   { path: `/`, element: <HomePage /> },
-//   { path: `/tutors`, element: <ReadTutorsList /> },
-//   { path: `/create-tutor`, element: <CreateTutor /> },
-//   { path: `/read-tutor/:id`, element: <ReadTutor /> },
-//   { path: `/update-tutor/:id`, element: <UpdateTutor/> },
-// ]);
+const router = createBrowserRouter([
+  { 
+    path: `/`, 
+    element: <HomePage /> 
+  },
+  { 
+    path: `/tutors`, 
+    element: <ReadTutorsList /> 
+  },
+  { 
+    path: `/create-tutor`, 
+    element: <CreateTutor /> 
+  },
+  { 
+    path: `/read-tutor/:id`, 
+    element: <ReadTutor /> 
+  },
+  { 
+    path: `/update-tutor/:id`, 
+    element: <UpdateTutor/> 
+  },
+], { basename: `/COC-MESA-Website/`});
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={HomePage} />
-        <Route path="/tutors" Component={ReadTutorsList} />
-        <Route path="/create-tutor" Component={CreateTutor} />
-        <Route path="/read-tutor/:id" Component={ReadTutor} />
-        <Route path="/update-tutor/:id" Component={UpdateTutor} />
-      </Routes>
-    </Router>
-  );
+  return <><RouterProvider router={router}/></>
 }
 
 export default App;
